4b2246baa5da7698fd5ced8793aad48ea93a5950
wobisheng <1804678189@qq.com>
2021-02-07 16:37:34 +0800

    Site updated: 2021-02-07 16:37:31

---
+++ js/sentence.js
@@ -0,0 +1,22 @@
+var number1 = Math.floor(Math.random() * 10);
+number1 = number1 + 1;
+if(number1 == 1)
+{document.write("只是我不小心试探你的温柔");}
+else if(number1 == 2)
+{document.write("我在前面像小丑一样做戏，而你却在背后笑着看着议论着。");}
+else if(number1 == 3)
+{document.write("曾经以为，拥有是不容易的；后来才知道，原来舍弃更难。");}
+else if(number1 == 4)
+{document.write("两个人的回忆那么多，可是看客却只剩下我一个。个人的回忆那么多，可是看客却只剩下我一个。");}
+else if(number1 == 5)
+{document.write("幸福它真的不容易，在你的背影里有我爱你");}
+else if(number1 == 6)
+{document.write("有时候，不是对方不在乎你，而是你把对方看的太重要。");}
+else if(number1 == 7)
+{document.write("有一种感觉比失恋还要痛苦，叫做自作多情。");}
+else if(number1 == 8)
+{document.write("我希望有一个人可以看穿我的逞强，保护我的脆弱。");}
+else if(number1 == 9)
+{document.write("有时候，最痛苦的不是失去，而是得到以后并不快乐。");}
+else if(number1 == 10)
+{document.write("不尝试着做些能力之外的事情，就永远无法成长。");}
