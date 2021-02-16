a18ba27bc1eaafedf58d729ef1616e1dff0e8d0c
wobisheng <1804678189@qq.com>
2021-02-16 20:39:32 +0800

    Site updated: 2021-02-16 20:39:28

--- js/sen.js
+++ js/sen.js
@@ -1,24 +1,2 @@
-var number1 = Math.floor(Math.random() * 11);
-number1 = number1 + 1;
-if(number1 == 1)
-{document.getElementById("sentence").innerHTML="这城市风很大，孤独的人总是晚回家";}
-else if(number1 == 2)
-{document.getElementById("sentence").innerHTML="我们都生活在阴沟里，但仍有人仰望星空";}
-else if(number1 == 3)
-{document.getElementById("sentence").innerHTML="曾经以为，拥有是不容易的；后来才知道，原来舍弃更难。";}
-else if(number1 == 4)
-{document.getElementById("sentence").innerHTML="人间不会有单纯的快乐，快乐总夹杂着烦恼和忧虑，人间也没有永远";}
-else if(number1 == 5)
-{document.getElementById("sentence").innerHTML="错落一身静宁，深海浮沉摘星";}
-else if(number1 == 6)
-{document.getElementById("sentence").innerHTML="就这样与你告别，借万里青山，以之为隔，世世不见。";}
-else if(number1 == 7)
-{document.getElementById("sentence").innerHTML="有一种感觉比失恋还要痛苦，叫做自作多情。";}
-else if(number1 == 8)
-{document.getElementById("sentence").innerHTML="活着不一定要鲜艳，但一定要有自己的颜色";}
-else if(number1 == 9)
-{document.getElementById("sentence").innerHTML="有时候，最痛苦的不是失去，而是得到以后并不快乐。";}
-else if(number1 == 10)
-{document.getElementById("sentence").innerHTML="不尝试着做些能力之外的事情，就永远无法成长。";}
-else if(number1 == 11)
-{document.getElementById("sentence").innerHTML="外界事物纷至沓来，我敞开所有感官来接纳，但来者全是客";}
+const sentences = ["这城市风很大，孤独的人总是晚回家", "我们都生活在阴沟里，但仍有人仰望星空","曾经以为，拥有是不容易的；后来才知道，原来舍弃更难。","人间不会有单纯的快乐，快乐总夹杂着烦恼和忧虑，人间也没有永远","错落一身静宁，深海浮沉摘星","就这样与你告别，借万里青山，以之为隔，世世不见。","有一种感觉比失恋还要痛苦，叫做自作多情。","活着不一定要鲜艳，但一定要有自己的颜色","有时候，最痛苦的不是失去，而是得到以后并不快乐。","不尝试着做些能力之外的事情，就永远无法成长","外界事物纷至沓来，我敞开所有感官来接纳，但来者全是客"];
+document.getElementById("sentence").innerHTML = sentences[Math.floor(Math.random() * sentences.length)];
