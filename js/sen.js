2900190de9f04565ebdd478865b3aefb50ec9e2f
inventor <inventorwobisheng@qq.com>
2023-05-15 20:21:00 +0800

    Site updated: 2023-05-15 20:20:52

--- js/sen.js
+++ js/sen.js
@@ -2,5 +2,6 @@ var xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", 'https://v1.hitokoto.cn/?encode=jsonv', false );
 xmlHttp.send( null );
 content_html = JSON.parse(xmlHttp.responseText);
-sentence = content_html['hitokoto']
+sentence = content_html['hitokoto'].replace('。','');
+if (sentence.length > 15){sentence = sentence.slice(0,20) + '</br>' + sentence.slice(20)}
 document.getElementById("sentence").innerHTML = sentence;
