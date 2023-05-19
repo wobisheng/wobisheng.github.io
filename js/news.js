a8fd4d8b19fea2db910bd8337fdb302fdc10919e
inventor <inventorwobisheng@qq.com>
2023-05-19 09:06:03 +0800

    Site updated: 2023-05-19 09:05:53

--- js/news.js
+++ js/news.js
@@ -1,3 +1,5 @@
+setInterval("refresh_news()",5000);
+function refresh_news(){
 var newshttp = new XMLHttpRequest();
 newshttp.open( "GET", 'https://service-muomqjyj-1302618926.bj.apigw.tencentcs.com/release/', false );
 newshttp.send( null );
@@ -12,7 +14,8 @@ document.getElementById("news1").innerHTML = html;
 html = "";
 	for(var i = 1; i <= 15; i++)
 	{
-		html += '<p title='+content_news['result']['2'][i-1]['contentPreview']+'>' + i + ':' + content_news['result']['2'][i-1]['title'] + '<p>';
+		html += '<p title='+content_news['result']['2'][i-1]['contentPreview']+'> ' + i + ':' + content_news['result']['2'][i-1]['title'] + '<p>';
 		html += '<br></br>';
 	}
 document.getElementById("news2").innerHTML = html;
+}
