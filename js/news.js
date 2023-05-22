698173f41ca9db9eac460230af7d4ffa66bce3cf
inventor <inventorwobisheng@qq.com>
2023-05-22 23:12:40 +0800

    Site updated: 2023-05-22 23:12:32

--- js/news.js
+++ js/news.js
@@ -1,5 +1,28 @@
-setInterval("refresh_news()",5000);
-function refresh_news(){
+n_time = 0
+while(document.getElementById("news1").innerHTML == '<img src="https://www.intogif.com/resource/image/loading/square-queue.gif">'&&n_time <= 10)
+{
+	try 
+	{
+		refresh_news();
+	}
+	catch
+	{
+		n_time += 1;
+	}
+}
+
+if(n_time >= 10)
+{
+	document.getElementById("news1").innerHTML = "新闻api出现问题";
+	document.getElementById("news2").innerHTML = "";
+}
+else
+{
+setInterval("refresh_news()",30000);
+}
+
+function refresh_news()
+{
 var newshttp = new XMLHttpRequest();
 newshttp.open( "GET", 'https://service-muomqjyj-1302618926.bj.apigw.tencentcs.com/release/', false );
 newshttp.send( null );
